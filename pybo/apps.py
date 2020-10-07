5a8422eb251ffdf55f8cd3acd9bb0ffb3afc9d40
Sim-JunHo <jkjk11590@hanmail.net>
2020-10-07 09:24:02 +0900

    Wikidocs Django clonecoding proj start

---
+++ pybo/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class PyboConfig(AppConfig):
+    name = 'pybo'
+"""wmic ComputerSystem Where Name=%COMPUTERNAME% Call Rename Name="SimJunHo"
+
+출처: https://didalsgur.tistory.com/39 [Took-Took]"""
