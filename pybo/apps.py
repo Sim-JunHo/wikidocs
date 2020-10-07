7536e867ed6a89f8db5d5b9160e725646cb7f986
Sim-JunHo <jkjk11590@hanmail.net>
2020-10-07 10:30:48 +0900

    learning how to connect git and pycharm

--- pybo/apps.py
+++ pybo/apps.py
@@ -3,6 +3,9 @@ from django.apps import AppConfig
 
 class PyboConfig(AppConfig):
     name = 'pybo'
-"""wmic ComputerSystem Where Name=%COMPUTERNAME% Call Rename Name="SimJunHo"
 
-출처: https://didalsgur.tistory.com/39 [Took-Took]"""
+"""
+wmic ComputerSystem Where Name=%COMPUTERNAME% Call Rename Name="SimJunHo"
+
+출처: https://didalsgur.tistory.com/39 [Took-Took]
+"""
