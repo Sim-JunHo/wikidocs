29edf55a8b11f0438b011c0b11f62da3be2b6ee6
Sim-JunHo <jkjk11590@hanmail.net>
2020-10-10 13:09:06 +0900

    2-04 done. get question list page

--- pybo/views.py
+++ pybo/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Question
 
 
 def index(req):
-    return HttpResponse("Welcome to Pybo.")
+    """
+    pybo 목록 출력
+    """
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list': question_list}
+    return render(req, 'pybo/question_list.html', context)
+
+
+def detail(req, question_id):
+    """
+    pybo 내용 출력
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(req, 'pybo/question_detail.html', context)
