112360d1ba041bb42b5fbfdac96ac0fa7bf6c619
Sim-JunHo <jkjk11590@hanmail.net>
2020-10-15 19:19:22 +0900

    2-10 Form done. Can make Question and Answer

--- pybo/views.py
+++ pybo/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .models import Question
 from django.utils import timezone
+from .forms import QuestionForm, AnswerForm
 
 
 def index(req):
@@ -27,5 +28,33 @@ def answer_create(req, question_id):
     pybo 답변 등록
     """
     question = get_object_or_404(Question, pk=question_id)
-    question.answer_set.create(content=req.POST.get('content'), create_date=timezone.now())
-    return redirect('pybo:detail', question_id=question.id)
+    if req.method == 'POST':
+        form = AnswerForm(req.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.save()
+            return redirect('pybo:detail', question_id=question.id)
+    else:
+        form = AnswerForm()
+
+    context = {'question': question, 'form': form}
+    return render(req, 'pybo/question_detail.html', context)
+
+
+def question_create(req):
+    """
+    pybo 질문등록
+    """
+    if req.method == 'POST':
+        form = QuestionForm(req.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('pybo:index')
+    else:
+        form = QuestionForm()
+    context = {'form': form}
+    return render(req, 'pybo/question_form.html', context)
